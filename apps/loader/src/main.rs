1167e8ebd45f5ece1e706fc582707784b716e9bf
LiuZhetan <liuzhetande@foxmail.com>
2023-11-21 23:39:55 +0800

    finish exercise4

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -4,7 +4,33 @@
 
 #[cfg(feature = "axstd")]
 use axstd::println;
+
+#[cfg(feature = "axstd")]
+use axstd::ax_terminate;
+
 const PLASH_START: usize = 0x22000000;
+
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe { ABI_TABLE[num] = handle; }
+}
+
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
+#[cfg(feature = "axstd")]
+fn abi_terminate() {
+    ax_terminate();
+}
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
@@ -40,4 +66,36 @@ fn main() {
         )}
         println!("Load payload ok!");
     }
+
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+    #[cfg(feature = "axstd")]
+    register_abi(SYS_TERMINATE, abi_terminate as usize);
+
+    println!("Execute app ...");
+    let arg0: u8 = b'A';
+
+    unsafe { core::arch::asm!("
+       li     t0, {abi_num}
+       slli   t0, t0, 3
+       la     t1, {abi_table}
+       add     t2, t1, t0
+       ld     t2, (t2)
+       jalr   t2
+       li     t3, {run_start}
+       jalr   t3
+       li     t0, {terminate_num}
+       slli   t0, t0, 3
+       add     t2, t1, t0
+       ld     t2, (t2)
+       jalr   t2
+       li     t3, {run_start}
+       jalr   t3",
+        run_start = const RUN_START,
+        abi_table = sym ABI_TABLE,
+        //abi_num = const SYS_HELLO,
+        abi_num = const SYS_PUTCHAR,
+        terminate_num = const SYS_TERMINATE,
+        in("a0") arg0,
+   )}
 }
