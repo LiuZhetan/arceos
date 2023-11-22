1f6d4bbf1685425a91f4498c5d74c427b2d504e0
LiuZhetan <liuzhetande@foxmail.com>
2023-11-22 21:15:18 +0800

    finish exercise5-6

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -8,6 +8,8 @@ use axstd::println;
 #[cfg(feature = "axstd")]
 use axstd::ax_terminate;
 
+use riscv::register::satp;
+
 const PLASH_START: usize = 0x22000000;
 
 const SYS_HELLO: usize = 1;
@@ -34,6 +36,12 @@ fn abi_terminate() {
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
+    // register abi
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+    #[cfg(feature = "axstd")]
+    register_abi(SYS_TERMINATE, abi_terminate as usize);
+
     let plash_ptr = PLASH_START as *const u64;
     let app_num = unsafe { plash_ptr.read_volatile() };
     println!("Load payload, app num: {} ...", app_num);
@@ -43,6 +51,7 @@ fn main() {
     // SBI(0x80000000) -> App <- Kernel(0x80200000)
     // 0xffff_ffc0_0000_0000
     const RUN_START: usize = 0xffff_ffc0_8010_0000;
+    const APP_RUN_START: usize = 0x4010_0000;
 
     for _ in 0..app_num {
         let app_size = unsafe { plash_ptr.read_volatile() }; 
@@ -58,20 +67,26 @@ fn main() {
         println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
 
         println!("Execute app ...");
+        println!("ABI_TABLE addr:{:#x}",unsafe {
+            ABI_TABLE.as_ptr() as usize
+        });
+
+        // switch aspace from kernel to app
+        unsafe { init_app_page_table(); }
+        unsafe { switch_app_aspace(); }
         // execute app
         unsafe { core::arch::asm!("
-           li     t2, {run_start}
-           jalr   t2", 
-           run_start = const RUN_START,
+            la     a7, {abi_table}
+            li     t2, {run_start}
+            jalr   t2", 
+            run_start = const APP_RUN_START,
+            abi_table = sym ABI_TABLE,
         )}
         println!("Load payload ok!");
     }
 
-    register_abi(SYS_HELLO, abi_hello as usize);
-    register_abi(SYS_PUTCHAR, abi_putchar as usize);
-    #[cfg(feature = "axstd")]
-    register_abi(SYS_TERMINATE, abi_terminate as usize);
-
+    /*
+    // run exercise4
     println!("Execute app ...");
     let arg0: u8 = b'A';
 
@@ -97,5 +112,32 @@ fn main() {
         abi_num = const SYS_PUTCHAR,
         terminate_num = const SYS_TERMINATE,
         in("a0") arg0,
-   )}
+   )}*/
 }
+
+//
+// App aspace
+//
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+
+unsafe fn init_app_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+    // 0x0000_0000..0x4000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+    // For App aspace!
+    // 0x4000_0000..0x8000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+
+unsafe fn switch_app_aspace() {
+    let page_table_root = APP_PT_SV39.as_ptr() as usize -
+    axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    
+    riscv::asm::sfence_vma_all();
+}
+
